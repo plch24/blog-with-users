9bee00bf9a4c34b06ed5c5b8ef57a0f90239128c
petar.milic <petar.milic@otaco.rs>
2021-02-27 10:26:09 +0100

    change email to user email

--- forms.py
+++ forms.py
@@ -12,7 +12,7 @@ class CreatePostForm(FlaskForm):
     submit = SubmitField("Submit Post")
 
 class RegisterForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired()])
+    email = StringField("User Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     name = StringField("Name", validators=[DataRequired()])
     submit = SubmitField("SIGN ME UP!")
